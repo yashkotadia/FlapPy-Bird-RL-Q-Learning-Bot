60fb84278728e50efa32df263f063a466d5afa5b
Yash Kotadia <yashkotadia@gmail.com>
2017-12-02 18:08:07 +0530

    Update plot-scores.py

--- plot-scores.py
+++ plot-scores.py
@@ -3,15 +3,20 @@ import random
 from matplotlib import pyplot as plt
 import pandas as pd
 
+# Open the scores file and load scores
 with open('scores.pkl', 'rb') as f:
 	Q = pickle.load(f)
 
-df  = pd.DataFrame(Q)
-rm = df.rolling(window=1000).mean()
-rs = df.rolling(window=1000).std()
-upper_band = rm + 2*rs
-lower_band = rm - 2*rs
-plt.figure(figsize=(15,10))
-plt.plot(df,'g.',rm,'b',upper_band,'r')
-plt.title('Scores')
+df  = pd.DataFrame(Q) # Create a dataframe of scores
+rm = df.rolling(window=500).mean() # Calculate rolling mean
+rs = df.rolling(window=500).std() # Calculate rolling standard deviation
+upper_band = rm + rs # The upper band in the graph to show how much scores deviate from mean
+
+# Plotting the graph using pyplot
+plt.semilogy(df,'g.',label='Scores')
+plt.semilogy(rm,'b',label='Mean')
+plt.semilogy(upper_band,'r',label='Standard Deviation')
+plt.legend()
+plt.grid(True, which='both')
+plt.title('Training Curve')
 plt.show()
